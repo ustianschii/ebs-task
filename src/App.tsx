3f9494df8a6a09a55c1c1ac71ce93560c524cb08
ustianschii <ustianschii@gmail.com>
2024-12-05 15:06:04 +0200

    fix: added redirect file for netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/Routes";
 import { CartProvider } from "./context/CartContext";
+import React from "react";
 
 function App() {
   return (
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import React from "react";
 
 import Home from "../pages/home/Home";
 import Cart from "../pages/cart/Cart";
